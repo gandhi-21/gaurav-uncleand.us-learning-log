0af66c2b554795b9d60a0e7e44fb9e085cff6723
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2020-05-25 18:16:36 -0400

    finished exercise 3.6

---
+++ full-stack-open-2020/part3/phonebook-backend/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebook = [
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        id: 2
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        id: 3
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        id: 4
+      },
+      {
+        "name": "gandhi",
+        "number": "435343534",
+        id: 5
+      }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+
+app.get('/info', (request, response) => {
+    const total = phonebook.length
+    response.send(`<p>Phonebook has info for ${total} people</p> <br> ${new Date()}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(400).json({
+            error: 'Id not found'
+        })
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebook.find(person => person.id === id)
+
+    if (!person) {
+        response.status(400).json({
+            error: 'Id does not exist'
+        })
+    } else {
+        phonebook = phonebook.filter(n => n.id !== id)
+        response.status(204).end()
+    }
+})
+
+const generateId = () => {
+    const maxId = phonebook.length > 0 ? Math.max(...phonebook.map(p => p.id)) : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body || !body.number || !body.name) {
+        response.status(400).json({
+            error: 'No content in the body!'
+        })
+    } else {
+        const check = phonebook.find(n => n.name === body.name)
+        if(check) {
+            response.status(400).json({
+                error: 'name must be unique'
+            })
+        } else {
+            const person = {
+                "name": body.name,
+                "number": body.number,
+                id: generateId()
+            }
+            phonebook = phonebook.concat(person)
+            response.json(person)
+        }
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
